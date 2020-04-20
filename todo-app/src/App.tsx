ec051dd353fb86c0e8b49595248fc4031832a534
min33sky <min33sky@naver.com>
2020-04-20 17:22:37 +0900

    Update: Todo

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -3,58 +3,53 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList, { Todo } from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // 고유값으로 사용될 id
   // ref를 사용하여 변수 담기 (랜더링과 상관 없는 값)
-  const nextId = useRef<number>(4);
+  const nextId = useRef<number>(2501);
 
-  const onInsert = useCallback(
-    (text: string) => {
-      const todo: Todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const onInsert = useCallback((text: string) => {
+    const todo: Todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    // ! 함수형 업데이트를 사용하면 의존배열에 상태값을 넣을 필요가 없어서
+    // ! 함수 재생성을 막아준다.
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
   const onRemove = useCallback(
     (id: number) => () => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      setTodos((todos) => todos.filter((todo) => todo.id !== id));
     },
-    [todos],
+    [],
   );
 
   const onToggle = useCallback(
     (id: number) => () => {
-      setTodos(
+      setTodos((todos) =>
         todos.map((todo) =>
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
       );
     },
-    [todos],
+    [],
   );
 
   return (
--- todo-app/src/components/TodoListItem.tsx
+++ todo-app/src/components/TodoListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import {
   MdCheckBoxOutlineBlank,
   MdCheckBox,
@@ -12,22 +12,29 @@ type TodoListItemProps = {
   todo: Todo;
   onRemove: (id: number) => () => void;
   onToggle: (id: number) => () => void;
+  style?: React.CSSProperties | undefined;
 };
 
-function TodoListItem({ todo, onRemove, onToggle }: TodoListItemProps) {
+function TodoListItem({ todo, onRemove, onToggle, style }: TodoListItemProps) {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div className={cn('checkbox', { checked })} onClick={onToggle(id)}>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 }
 
-export default TodoListItem;
+// ! memo를 적용하면 todo, onRemove, onToggle이 바뀌지 않는 한 리랜저링 되지 않는다.
+export default memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
